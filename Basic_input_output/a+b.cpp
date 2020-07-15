a87a644d320ce1245d353719723d6401b20b868d
SanjoyPator1 <sanjoyofficialp@gmail.com>
2020-07-15 13:25:04 +0530

    first test

---
+++ Basic_input_output/a+b.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include <boost/multiprecision/cpp_int.hpp> 
+using namespace boost::multiprecision; 
+using namespace std;
+
+
+//this method is used for lard datas
+int main(){
+    
+    int512_t a,b;
+    
+    while(cin>>a>>b){
+    
+    
+    cout<<a+b<<endl;
+    }
+
+    return 0;
+    
+    /*
+    Input Format
+
+Each line contains two integers, A
+and B. One input file may contain several pairs where
+0<=A,B<=10**(98)
+.
+
+
+
+Output a single integer per line - The sum of A
+and B
+E.g - 
+1 2      - 3
+2 5      - 7
+10  14   - 24
+
+
+    
+    */
+}
