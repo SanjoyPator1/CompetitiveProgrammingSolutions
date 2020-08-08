3fd520bcf63d28a8a039c7af017043ee2000f661
Sanjoy Pator <sanjoypator1@gmail.com>
2020-08-08 23:49:35 +0530

    Create Day24_remove_dublicate_linked_list.cpp

---
+++ Basic_input_output/Day24_remove_dublicate_linked_list.cpp
@@ -0,0 +1,104 @@
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+class Solution{
+public:
+
+    Node* removeDuplicates(Node *head) {
+        //Write your code here
+
+        Node *p, *q;
+        p = head;
+
+        if(head==NULL) {
+            return NULL;
+        }
+
+        while (p->next != NULL) {
+            q = p->next;
+
+
+            //checking
+            if (p->data == q->data) {
+                q->data = NULL;
+
+                //again check
+                if(q->next==NULL){
+                    p->next = NULL;
+                }else{
+                    p->next = q->next;
+                }
+
+            }else{
+                p=p->next;
+            }
+
+        }
+
+        return head;
+
+    }
+
+    Node* insert(Node *head,int data)
+    {
+        Node* p=new Node(data);
+        if(head==NULL){
+            head=p;
+
+        }
+        else if(head->next==NULL){
+            head->next=p;
+
+        }
+        else{
+            Node *start=head;
+            while(start->next!=NULL){
+                start=start->next;
+            }
+            start->next=p;
+
+        }
+        return head;
+
+
+    }
+    void display(Node *head)
+    {
+        Node *start=head;
+        while(start)
+        {
+            cout<<start->data<<" ";
+            start=start->next;
+        }
+    }
+};
+
+int main()
+{
+    Node* head=NULL;
+    Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }
+    head=mylist.removeDuplicates(head);
+
+    mylist.display(head);
+
+}
