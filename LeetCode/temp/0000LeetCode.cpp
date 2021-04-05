0653e6e27e67f91e582844a6cba8d5d38ab42c34
SanjoyPator1 <sanjoypator1@gmail.com>
2021-04-05 15:04:35 +0530

    LeetCode init

---
+++ LeetCode/temp/0000LeetCode.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Move all negative numbers to beginning and positive to end with constant extra space
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int in, out;
+	cin >> in;
+
+	out = in * 2;
+
+	cout << "The output is " << out << endl;
+
+
+}
